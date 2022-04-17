61ea932743733b1c9faa733ea34677fd27a3c65b
christian <christianmambu2@gmail.com>
2022-04-17 15:23:18 +0200

    second commit

--- app/src/main/java/com/example/map/MapsActivity.java
+++ app/src/main/java/com/example/map/MapsActivity.java
@@ -34,7 +34,7 @@ private ActivityMapsBinding binding;
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
-     * -26.153635018486717, 27.981678706334883
+     * -26.153635018486717, 27.981678706334883 wsfgerwfg
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
